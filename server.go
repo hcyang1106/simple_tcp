c0e3dce87d093cf9ec5e6d92325d748701b4844a
howard <howard.yang@kiwigame.com>
2022-05-07 15:00:36 +0800

    first commit

---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	_ "io"
+	"net"
+)
+
+func process(conn net.Conn) {
+	//循環接收client的訊息
+	defer conn.Close() // close conn
+
+	for {
+
+		fmt.Printf("server在等待client %s 發送訊息\n", conn.RemoteAddr().String())
+
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf) // 阻塞
+		if err != nil {
+			fmt.Printf("client退出 err=%v", err)
+			return
+		}
+		// show message
+		fmt.Print(string(buf[:n]))
+	}
+
+}
+
+func main() {
+	fmt.Println("server listening....")
+
+	listen, err := net.Listen("tcp", "0.0.0.0:8888")
+	if err != nil {
+		fmt.Println("listen error, err=", err)
+		return
+	}
+	defer listen.Close()
+
+
+	for {
+		//等待client連接
+		fmt.Println("等待client連接....")
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Accept() error, err=", err)
+		} else {
+			fmt.Printf("Accept() success conn=%v, client ip=%v\n", conn, conn.RemoteAddr().String())
+		}
+		go process(conn)
+	}
+}
